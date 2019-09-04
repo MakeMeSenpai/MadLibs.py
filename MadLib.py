6c1261434829b29c9743edcfc0a339982c52f61f
Anthony <sean.a.protho1@gmail.com>
2019-09-04 14:15:06 -0700

    stuff

--- MadLib.py
+++ MadLib.py
@@ -3,15 +3,14 @@ import os
 import time
 
 def MadLibs():
+    print("Please do not type unless prompted")
     name = input("Hi! What is your name? ")
-    places = []
-    places.append[] = input("Name a place(store): ")
-    places.append[] = input("Name some other places(home): ")
-    places.append[] = input()
-    place.append[] = input()
-    place.append[] = input()
-    items1 = input("Name a plural item(grapes): ")
-    items2 = input("Name a another plural item(mind): ")
+    place = list()
+    place.append(input("Name a place(store): "))
+    place.append(input("Name some other places(home): "))
+    item = list()
+    item.append(input("Name a plural item(grapes): ")) 
+    item.append(input("Name a another plural item(mind): "))
     date = input("Name a set of time(months): ")
     descriptor = input("Name an adjustive(soggy): ")
     action = input("Name an action(ran): ")
@@ -20,24 +19,21 @@ def MadLibs():
     os.system("clear")
     print("*** Loading Results!!! ***")
     time.sleep(2)
-    print("###########", end="")
+    print("###############", end="")
     time.sleep(1)
-    print("####", end="")
+    print("########", end="")
     time.sleep(2)
-    print("######")
+    print("##########")
     print("Story Time; ")
     print(f"""
-        {name} went to the {place1} to buy lots and lots of {items1}. 
-        However when {name} got the {items1}, they lost their {items2} over 
+        {name} went to the {place[0]} to buy lots and lots of {item[0]}. 
+        However when {name} got the {item[0]}, they lost their {item[1]} over 
         the ridiculous price, and how they would now have to go {date} 
-        with a bunch of {descriptor} {items1}. {name} {action}
-        {place2} and {feeling}. The End.""")
+        with a bunch of {descriptor} {item[0]}. {name} {action}
+        {place[1]} and {feeling}. The End.""")
 MadLibs()
 
 #Thanks Kevin for Helping me with same line print statements!
-
-#To-Do list
-#use a list, tupel, or dictionary
 
 #Stretch
 #Handles improper user input
